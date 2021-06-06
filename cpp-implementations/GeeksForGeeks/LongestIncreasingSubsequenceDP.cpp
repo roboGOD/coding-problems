d3ef351e2de53d1a190072572697925431e09434
roboGOD <ishwer.kumar666@gmail.com>
2021-06-06 09:11:07 +0530

    Longest Increasing subsequence best solution

--- cpp-implementations/GeeksForGeeks/LongestIncreasingSubsequenceDP.cpp
+++ cpp-implementations/GeeksForGeeks/LongestIncreasingSubsequenceDP.cpp
@@ -2,12 +2,69 @@
 #include <vector>
 using namespace std;
 
+int binarySearch(int a[], int l, int r, int val) {
+  int m = r;
+  int pL = -1, pR = -1;
+  while (l < r && !(pL == l && pR == r) && a[m] != val) {
+    pL = l;
+    pR = r;
+    m = l + (r - l + 1) / 2;
+    if(a[m] >= val) {
+      r = m;
+    } else {
+      l = m;
+    }
+  }
+
+  return a[m] == val ? m : (a[l] > val ? -1 : l);
+}
+
+void display(int n, int a[]) {
+  for(int i =0; i < n; i++) {
+    cout << a[i] << " ";
+  }
+  cout << endl;
+}
+
+/**
+ * 
+ * Binary Search
+ * 
+ * O(nlogn)
+ * 
+ */ 
+int longestSubsequenceBest(int n, int a[]) {
+  if(n <= 0) {
+    return 0;
+  }
+
+  int v[n+1];
+  for(int i = 0; i<=n; i++) {
+    v[i] = 0;
+  }
+  v[0] = a[0];
+  int vlen = 1;
+  for(int i=1; i<n; i++) {
+    if(a[i] < v[0]) {
+      v[0] = a[i];
+    } else if(a[i] > v[vlen-1]) {
+      v[vlen] = a[i];
+      vlen++;
+    } else {
+      v[binarySearch(v, 0, vlen-1, a[i]) + 1] = a[i];
+    }
+  }
+
+  return vlen;
+}
 
 /**
  * 
  * Dynamic Programming
  * 
- **/
+ * O(n^2)
+ * 
+ */ 
 int longestSubsequenceDP(int n, int a[]) {
   int results[n];
   int maxI = 0;
@@ -68,6 +125,6 @@ int main() {
     cin >> a[i];
   }
 
-  cout << longestSubsequenceDP(n, a) << endl;
+  cout << longestSubsequenceBest(n, a) << endl;
   return 1;
 }
