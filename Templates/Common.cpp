392056df67a8bb6612958c312c6c2d08b28771a7
roboGOD <ishwer.kumar666@gmail.com>
2021-07-03 22:23:09 +0530

    create templates

---
+++ Templates/Common.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define lli long long int
+#define pb push_back
+
+using namespace std;
+
+int main() {
+
+  return 0;
+}
---
+++ Templates/Kickstart.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define lli long long int
+#define pb push_back
+
+using namespace std;
+
+int main() {
+  lli t;
+
+  cin >> t;
+  for(int _=0; _<t; _++) {
+
+    cout << "Case #" << _+1 << ": " << 0 << endl;
+  }
+}
