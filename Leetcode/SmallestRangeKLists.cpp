57f1cbac08e10d42b6752cb503c996b96cd724e4
roboGOD <ishwer.kumar666@gmail.com>
2021-12-03 09:29:04 +0530

    not done 2

---
+++ Leetcode/SmallestRangeKLists.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// [01 02 04 09 13]
+// [01 03 05 10 15]
+// [09 12 24 30]
+
+// 01 02 .. 04 .. .. .. .. 09 .. .. .. 13 .. ..
+// 01 .. 03 .. 05 .. .. .. .. 10 .. .. .. .. 15
+// .. .. .. .. .. .. .. .. 09 .. .. 12 .. .. .. .. .. 24 .. 30
+
+class Solution {
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        
+    }
+};
